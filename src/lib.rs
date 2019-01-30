99b2d4db0c196e27b456c5ade3350a49f900c168
Jakub Pastuszek <jpastuszek@gmail.com>
2019-01-30 17:07:22 +0000

    _with variants

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,69 @@ pub trait WithContext<C> {
 pub trait ResultErrorWhile<C> {
     type ContextError;
     fn error_while(self, context: C) -> Self::ContextError;
+    fn error_while_with<F>(self, context: F) -> Self::ContextError where F: FnOnce() -> C;
 } 
 
 impl<O, E, C> ResultErrorWhile<C> for Result<O, E> where E: WithContext<C, ContextError = E> {
     type ContextError = Self;
     fn error_while(self, context: C) -> Self {
         self.map_err(|e| e.with_context(context))
+    }
+
+    fn error_while_with<F>(self, context: F) -> Self::ContextError where F: FnOnce() -> C {
+        self.map_err(|e| e.with_context(context()))
+    }
+}
+
+#[derive(Debug)]
+pub struct RootCause<E>(E);
+
+impl<E> RootCause<E> {
+    pub fn unwrap(self) -> E {
+        self.0
+    }
+}
+
+impl<E> Display for RootCause<E> where E: Display {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<E> Error for RootCause<E> where E: Error {
+    fn description(&self) -> &str {
+        self.0.description()
+    }
+
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        self.0.source()
+    }
+}
+
+impl<E, C> WithContext<C> for RootCause<E> {
+    type ContextError = ErrorContext<RootCause<E>, C>;
+    fn with_context(self, context: C) -> ErrorContext<RootCause<E>, C> {
+        ErrorContext(self, context)
+    }
+}
+
+pub trait ToRootCause<T> {
+    fn to_root_cause(self) -> RootCause<T>;
+}
+
+impl<T> ToRootCause<T> for T { 
+    fn to_root_cause(self) -> RootCause<Self> {
+        RootCause(self)
+    }
+}
+
+pub trait MapRootCause<O, E> {
+    fn map_root_cause(self) -> Result<O, RootCause<E>>;
+}
+
+impl<O, E> MapRootCause<O, E> for Result<O, E> {
+    fn map_root_cause(self) -> Result<O, RootCause<E>> {
+        self.map_err(ToRootCause::to_root_cause)
     }
 }
 
@@ -65,11 +122,16 @@ impl<E, C> WrapContext<C> for E where E: Error {
 
 pub trait MapErrorContext<O, E, C> {
     fn map_error_context(self, context: C) -> Result<O, ErrorContext<E, C>>;
+    fn map_error_context_with<F>(self, context: F) -> Result<O, ErrorContext<E, C>> where F: FnOnce() -> C;
 }
 
 impl<O, E, C> MapErrorContext<O, E, C> for Result<O, E> where E: WrapContext<C, ContextError = ErrorContext<E, C>> {
     fn map_error_context(self, context: C) -> Result<O, ErrorContext<E, C>> {
         self.map_err(|e| e.wrap_context(context))
+    }
+
+    fn map_error_context_with<F>(self, context: F) -> Result<O, ErrorContext<E, C>> where F: FnOnce() -> C {
+        self.map_err(|e| e.wrap_context(context()))
     }
 }
 
@@ -82,16 +144,15 @@ where
     body().map_err(|e| e.with_context(context))
 }
 
-// /// Executes closure with problem_while_with context
-// pub fn in_context_of_with<O, F, M, B>(msg: F, body: B) -> Result<O, Problem>
-// where
-//     F: FnOnce() -> M,
-//     M: Display,
-//     B: FnOnce() -> Result<O, Problem>,
-// {
-//     body().problem_while_with(msg)
-// }
-
+/// Executes closure with with_context context function called on Err wariant
+pub fn in_context_of_with<O, E, C, CE, F, M, B>(context: F, body: B) -> Result<O, CE>
+where
+    F: FnOnce() -> C,
+    E: WithContext<C, ContextError = CE>,
+    B: FnOnce() -> Result<O, E>,
+{
+    body().map_err(|e| e.with_context(context()))
+}
 
 #[cfg(test)]
 mod tests {
@@ -143,11 +204,12 @@ mod tests {
     fn test_in_context_of() {
         use std::io::{Error, ErrorKind};
 
-        let err = in_context_of("processing fish sticks".to_string(), || {
+        let err = in_context_of("opening file".to_string(), || {
             let err: Result<(), Error> = Err(Error::new(ErrorKind::Other, "file is no good"));
-            err.map_error_context("opening file".to_string())
+            //err.map_error_context("opening file".to_string())
+            err.map_root_cause()
         });
 
-        assert_eq!(err.unwrap_err().to_string(), "while processing fish sticks got error: while opening file got error: file is no good");
+        assert_eq!(err.map_error_context("processing fish sticks".to_string()).unwrap_err().to_string(), "while processing fish sticks got error: while opening file got error: file is no good");
     }
 }
